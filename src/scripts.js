e01acb9ee70f67aa5b13263a132d5b451a2c2e14
Laura Long <lauralonggone@gmail.com>
2024-02-23 17:39:33 -0700

    Remove animateMotivation func and pass userID through the storeMotivation func

--- src/scripts.js
+++ src/scripts.js
@@ -180,41 +180,18 @@ function grabHydrationData(selectedDate, ounces) {
             numOunces: ounces}
             postData(userPost)
  }
- 
-function animateMotivation(inputValue) {
-    storeMotivation(inputValue)
-    let value = inputValue
-    switch(value) {
-      case '0': 
-        takeOff.style.animationName = "anim-0";
-        break;
-      case '2': 
-        takeOff.style.animationName = "anim-1";
-        break;
-      case '4': 
-        takeOff.style.animationName = "anim-2";
-        break;
-      case '6': 
-        takeOff.style.animationName = "anim-3";
-        break;
-      case '8': 
-        takeOff.style.animationName = "anim-4";
-        break;
-      case '10': 
-        takeOff.style.animationName = "anim-5";
-        break;
-    }
-}
 
-function storeMotivation(inputValue) {
+function storeMotivation(userID, inputValue) {
     let dateN = new Date()
     let motivation = {
-        userID: randomUserId,
-        motivation: inputValue,
-        date: dateN
-    }
-    return motivation
-}
+      userID: userID,
+      motivation: inputValue,
+      date: dateN
+    };
+
+    return motivation;
+  }
+
 
 export {
     getUserInfo,
@@ -233,5 +210,5 @@ export {
     calculateAvgQuality,
     calculateAvgHours,
     grabHydrationData,
-    animateMotivation
+    storeMotivation
 }
